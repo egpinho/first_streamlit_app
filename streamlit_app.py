00f78e264600bf3693e3b6ca3903cb605e2d0941
egpinho <egpinho@gmail.com>
2023-06-29 17:15:26 -0300

    Update streamlit_app.py
    Adding pandas call to normalize data and show it to a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,8 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!") 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# normalizes the json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# turns the normalized reponse into a table to show on screen
+streamlit.dataframe(fruityvice_normalized)
