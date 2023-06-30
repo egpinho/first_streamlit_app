34a0e65e6b460d822147400907448fd590d26466
egpinho <egpinho@gmail.com>
2023-06-30 01:03:58 -0300

    Update streamlit_app.py
    adding str function to test

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if streamlit.button('Get Fruit Load List'):
 def insert_row_snowflake(new_fruit):
   with my_cnxa.cursor() as my_cura:
     #my_cura.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
-    my_cur.execute("insert into FRUIT_LOAD_LIST values ('"+ new_fruit +"')"
+    my_cur.execute("insert into FRUIT_LOAD_LIST values ('"+ str(new_fruit) +"')"
     return "Thanks for adding " + new_fruit
 
 
