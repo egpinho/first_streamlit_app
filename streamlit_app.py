d23072d99877769b26ec0967f55bb8636e616087
egpinho <egpinho@gmail.com>
2023-06-30 00:56:20 -0300

    Update streamlit_app.py
    cnxa error

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if streamlit.button('Get Fruit Load List'):
 
 #allow user to add fruit
 def insert_row_snowflake(new_fruit):
-  with my_cnxa.cursor() as my_cura:
+  with my_cnx.cursor() as my_cura:
     my_cura.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
     #my_cur.execute("insert into FRUIT_LOAD_LIST values ('"+ add_my_fruit +"')"
     return "Thanks for adding " + new_fruit
