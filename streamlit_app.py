2c05b733a169d8d9b175285f6bf0f817ca139f4f
egpinho <egpinho@gmail.com>
2023-06-30 00:57:34 -0300

    Update streamlit_app.py
    back to correct cnxa

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if streamlit.button('Get Fruit Load List'):
 
 #allow user to add fruit
 def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cura:
+  with my_cnxa.cursor() as my_cura:
     my_cura.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
     #my_cur.execute("insert into FRUIT_LOAD_LIST values ('"+ add_my_fruit +"')"
     return "Thanks for adding " + new_fruit
@@ -73,7 +73,7 @@ def insert_row_snowflake(new_fruit):
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cnxa = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
   
